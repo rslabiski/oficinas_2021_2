8b06d6cdf187294ae94aa4b981b211d31d70db30
Andrea Oquendo <oquendo.3455@gmail.com>
2021-11-04 22:24:15 -0300

    classe Jogador adicionada

---
+++ Jogo/Jogador.h
@@ -0,0 +1,13 @@
+#ifndef JOGADOR_H
+#define JOGADOR_H
+
+#include "Veiculo.hpp"
+
+class Jogador: public Veiculo {
+    private:
+        int i;
+    public:
+        Jogador();
+};
+
+#endif
--- Jogo/JogoNotebook.h
+++ Jogo/JogoNotebook.h
@@ -19,17 +19,21 @@ private:
     char dificuldade; // Variavel de controle para criação dos elementos
     bool fimDoJogo; //Variavel de controle do loop do jogo
     bool vitoria; //Variavel de controle da vitoria do jogador
+    int **percorrido;
 
 public:
-    JogoNotebook ();
-    ~JogoNotebook ();
+    JogoNotebook();
+    ~JogoNotebook();
 
+    void setup();
     void executar(); //Execução do jogo
     void capturaEntrada(); //Captura entrada do joystick e altera velocidade do jogador
     void atualizar(); //Atualiza os elementos do jogo
     void renderizar(); //Renderiza no Display e Matriz
     void acoesVitoria(); //Acoes quando o jogador Vencer
     void acoesDerrota(); //Acoes quadno o jogador Perder
+
+    void loop();
 };
 
 #endif
